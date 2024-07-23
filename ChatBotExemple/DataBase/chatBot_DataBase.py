e5433fa650c9fdca2992896617d44cd899a2fa4a
GustavoAssuncao02 <94201266+GustavoAssuncao02@users.noreply.github.com>
2024-07-23 09:18:16 -0300

    Preparing to populate the database

--- ChatBotExemple/DataBase/chatBot_DataBase.py
+++ ChatBotExemple/DataBase/chatBot_DataBase.py
@@ -3,47 +3,91 @@ import json
 from datetime import datetime
 
 
-timeframe = '2015-01'
+timeframe = "2015-01"
 sql_transaction = []
 
-connection = sqlite3.connect('{} .db'.format(timeframe))
+connection = sqlite3.connect("{} .db".format(timeframe))
 c = connection.cursor()
 
+
 def create_table():
-    c.execute("""CREATE TABLE IF NOT EXISTS parent_reply(parente_if TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)""")
-    
-    
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS parent_reply(parente_if TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)"""
+    )
+
+
 def format_data(data):
-    data = data.replace("\n"," newlinechar ") .replace("\r", "newlinechar ") .replace('"', "'")
+    data = (
+        data.replace("\n", " newlinechar ")
+        .replace("\r", "newlinechar ")
+        .replace('"', "'")
+    )
     return data
 
-def find_parents(pid):
+
+def find_existing_score(pid):
     try:
-        sql = "SELECT comment,score FROM parent_reply WHERE comment_id = '{}' LIMIT 1".format(pid)
+        sql = "SELECT score FROM parent_reply WHERE perent_id = '{}' LIMIT 1".format(
+            pid
+        )
         c.execute(sql)
         result = c.fetchone()
         if result != None:
             return result[0]
-        else: 
+        else:
             return False
     except Exception as e:
-    #print("find_parent",e)
-        return False 
+        # print("find_parent",e)
+        return False
+
+def accetable(data):
+    if len(data.split(' ')) > 50 or len(data) < 1:
+        return False
+    elif len(data) > 1000:
+        return False
+    elif data == '[deleted]' or data == '[removed]':
+        return False
+    else:
+        return True
     
+def find_parents(pid):
+    try:
+        sql = "SELECT comment FROM parent_reply WHERE comment_id = '{}' LIMIT 1".format(
+            pid
+        )
+        c.execute(sql)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else:
+            return False
+    except Exception as e:
+        # print("find_parent",e)
+        return False
+
+
 if __name__ == "__main__":
-        create_table()
-        row_counter = 0
-        paired_rows = 0
-        
-        with open("C:/Users/Gustavo/Documents/TI/Projetos/Chatbot-Exemple/Chat-Bot-Example/Data-ChatbotExemple/RC_2015-01", buffering=1000) as f:
+    create_table()
+    row_counter = 0
+    paired_rows = 0
 
-            for row in f:
-                row_counter += 1
-                row = json.loads(row)
-                parent_id = row["parent_id"]
-                body = format_data(row['body'])
-                created_utc = row ['created_utc']
-                score = row['score']
-                subreddit = row["subreddit"]
+    with open(
+        "C:/Users/Gustavo/Documents/TI/Projetos/Chatbot-Exemple/Chat-Bot-Example/Data-ChatbotExemple/RC_2015-01",
+        buffering=1000,
+    ) as f:
 
-                parent_data = find_parents(parent_id)
+        for row in f:
+            row_counter += 1
+            row = json.loads(row)
+            parent_id = row["parent_id"]
+            body = format_data(row["body"])
+            created_utc = row["created_utc"]
+            score = row["score"]
+            subreddit = row["subreddit"]
+
+            parent_data = find_parents(parent_id)
+
+            if score >= 2:
+                existing_comment_score = find_existing_score(parent_id)
+                if existing_comment_score:
+                    if score > existing_comment_score:
